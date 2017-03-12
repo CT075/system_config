e4a22ea94f939295d15786c3e40f0967433253f2
Cameron Wong <cjwong@andrew.cmu.edu (lancer)>
2017-03-12 17:35:19 -0400

    didn't add everything

---
+++ dotfiles/hosts/andrew/aliases.sh
@@ -0,0 +1,21 @@
+export MODULE_NAME="andrew_aliases"
+export MODULE_DEPS=()
+
+module_exec() {
+  alias killz='killall -9 '
+  alias hidden='ls -a | grep "^\..*"'
+  alias rm='rm -v'
+  alias cp='cp -v'
+  alias mv='mv -v'
+  alias shell='ps -p $$ -o comm='
+  alias sml='rlwrap sml'
+  alias math='rlwrap MathKernel'
+  alias coin='rlwrap coin'
+
+  alias ll='ls -l'
+  alias la='ls -a'
+
+  alias cc='gcc -Wall -W -ansi -pedantic -O2 '
+  alias valgrind-leak='valgrind --leak-check=full --show-reachable=yes'
+}
+
---
+++ dotfiles/hosts/andrew/scripts.sh
@@ -0,0 +1,26 @@
+# Bits and pieces taken from other people's bashrc files on andrew afs
+
+export MODULE_NAME="andrew_util"
+export MODULE_DEPS=()
+
+module_exec() {
+
+mesg n
+
+afsperms(){ find $1 -type d -exec fs sa {} $2 $3 \; ; }
+get_cs_afs_access() {
+  # Script to give a user with an andrew.cmu.edu account access to cs.cmu.edu
+  # See https://www.cs.cmu.edu/~help/afs/cross_realm.html for information.
+
+  # Get tokens. This might create the user, but I'm not sure that that's
+  # reliable, so we'll also try to do pts createuser.
+  aklog cs.cmu.edu
+
+  pts createuser $(whoami)@ANDREW.CMU.EDU -cell cs.cmu.edu 2>&1 | grep -v "Entry for name already exists"
+
+  aklog cs.cmu.edu
+
+  echo "Be sure to add aklog cs.cmu.edu & to your ~/.bashrc"
+}
+}
+
