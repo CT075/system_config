56ae2a35c23a3039a893b4123ced2daaa2267f69
Cameron Wong <cjwong@andrew.cmu.edu (lancer)>
2017-04-19 11:20:45 -0400

    change prompt on andrew

--- dotfiles/hosts/andrew/main.sh
+++ dotfiles/hosts/andrew/main.sh
@@ -11,7 +11,7 @@ export PATH=$PATH:/usr/lib/smlnj/bin
 export PATH=$PATH:/afs/andrew/course/15/150/bin
 
 # prompt/etc
-PROMPT='%D{[%H:%M:%S]}%F{yellow}%n%F{default}@%F{green}%m%f%F{default}:%(4~|%-1~/.../%2~|%3~)$ '
+PROMPT='%D{[%H:%M:%S]}%F{yellow}%n%F{default} at %F{green}%m%f%F{default}:%(4~|%-1~/.../%2~|%3~)$ '
 
 # Allow us to use github properly
 unset SSH_ASKPASS
