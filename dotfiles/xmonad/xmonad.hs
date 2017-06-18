2052149937a6ca02eafcf7e004f9af6fe2ba1256
Cameron Wong <cjwong@andrew.cmu.edu (lancer)>
2017-06-18 07:42:50 -0400

    xmonad stuff

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -2,5 +2,44 @@
 import XMonad
 import XMonad.Config.Gnome
 
-main = xmonad gnomeConfig
+import XMonad.Actions.CycleWS as CWS
+import XMonad.Layout.Tabbed as Tab
+
+import XMonad.Util.EZConfig as EZ
+
+import Data.Map as M
+
+main =
+  xmonad $ gnomeConfig
+  { terminal = "gnome-terminal"
+  , workspaces = myWorkspaces
+  , modMask = myModMask
+  , layoutHook = myLayout
+  }
+  `EZ.additionalKeysP`
+  bindings
+
+-- mod4Mask is win-super key
+myModMask = mod4Mask
+
+myWorkspaces :: [String]
+myWorkspaces =
+  [ "Quanta"
+  , "Exia"
+  , "Kyrios"
+  , "Cherudim"
+  , "Seraphim"
+  , "Zabanya"
+  , "Arios"
+  , "Harute"
+  , "Astraea"
+  ]
+
+myLayout = Tab.simpleTabbed
+
+bindings :: [(String, X ())]
+bindings =
+  [ ("C-M-<right>", CWS.nextWS)
+  , ("C-M-<left>", CWS.prevWS)
+  ]
 
