e40c043e717a17ea431f997d9d19dfef0a85d887
Cameron Wong <cam@caster>
2017-11-29 17:59:51 -0500

    caster

---
+++ dotfiles/hosts/caster/aliases.sh
@@ -0,0 +1,14 @@
+export MODULE_NAME="lancer_aliases"
+export MODULE_DEPS=()
+
+module_exec() {
+  alias pacman="sudo pacman"
+  alias sml="rlwrap sml"
+  alias ll="ls -l"
+  alias la="ls -a"
+  alias rebootwifi="sudo systemctl restart wpa_supplicant; sudo systemctl restart NetworkManager"
+  alias arm-gcc="/opt/devkitARM/bin/arm-none-eabi-gcc"
+  alias arm-gcc2="/opt/devkitARM/bin/arm-none-eabi-gcc-5.3.0"
+  alias arm-as="/opt/devkitARM/bin/arm-none-eabi-as"
+}
+
