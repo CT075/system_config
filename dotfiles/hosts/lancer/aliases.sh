d89174d01998264f6bb1406a71f8834dfae62272
Cameron Wong <cam@caster>
2018-01-09 19:29:14 -0500

    update some stuff

--- dotfiles/hosts/lancer/aliases.sh
+++ dotfiles/hosts/lancer/aliases.sh
@@ -4,6 +4,7 @@ export MODULE_DEPS=()
 module_exec() {
   alias pacman="sudo pacman"
   alias sml="rlwrap sml"
+  alias smlnj="sml"
   alias ll="ls -l"
   alias la="ls -a"
   alias rebootwifi="sudo systemctl restart wpa_supplicant; sudo systemctl restart NetworkManager"
