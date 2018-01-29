d5be036126440c6ce5573485fdba1adb09aee4a2
Cameron Wong <cam@caster>
2018-01-29 12:43:27 -0500

    adjust for 418 ispc

--- dotfiles/hosts/andrew/main.sh
+++ dotfiles/hosts/andrew/main.sh
@@ -10,6 +10,7 @@ export PATH=$PATH:/afs/andrew/course/15/122/bin
 export PATH=$PATH:/usr/lib/smlnj/bin
 export PATH=$PATH:/afs/andrew/course/15/150/bin
 export PATH=$PATH:/afs/cs/academic/class/15210-s17/mlton-spoonhower/build/bin
+export PATH=$PATH:/usr/local/depot/ispc-v1.91-linux
 
 # prompt/etc
 
