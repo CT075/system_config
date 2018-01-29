3727b7ef089d182a264be09056c0082b6ea65814
Cameron Wong <cam@caster>
2018-01-29 12:45:23 -0500

    typo

--- dotfiles/hosts/andrew/main.sh
+++ dotfiles/hosts/andrew/main.sh
@@ -10,7 +10,7 @@ export PATH=$PATH:/afs/andrew/course/15/122/bin
 export PATH=$PATH:/usr/lib/smlnj/bin
 export PATH=$PATH:/afs/andrew/course/15/150/bin
 export PATH=$PATH:/afs/cs/academic/class/15210-s17/mlton-spoonhower/build/bin
-export PATH=$PATH:/usr/local/depot/ispc-v1.91-linux
+export PATH=$PATH:/usr/local/depot/ispc-v1.9.1-linux
 
 # prompt/etc
 
