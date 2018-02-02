eada56718b84974092d0e97327e63c59cf63acac
CT075 <camtech075@gmail.com>
2018-02-02 13:54:26 -0500

    add cuda 2

--- dotfiles/hosts/andrew/main.sh
+++ dotfiles/hosts/andrew/main.sh
@@ -13,6 +13,8 @@ export PATH=$PATH:/afs/cs/academic/class/15210-s17/mlton-spoonhower/build/bin
 export PATH=$PATH:/usr/local/depot/ispc-v1.9.1-linux
 export PATH=$PATH:/usr/local/depot/cuda-8.0/bin
 
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/depot/cuda-8.0/lib64/
+
 # prompt/etc
 
 setopt PROMPT_SUBST
