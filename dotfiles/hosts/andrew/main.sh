f2a819ae37e801be048c3329b9866d90c4f99e0e
Cameron Wong <cam@caster>
2018-02-02 13:38:17 -0500

    cuda

--- dotfiles/hosts/andrew/main.sh
+++ dotfiles/hosts/andrew/main.sh
@@ -11,6 +11,7 @@ export PATH=$PATH:/usr/lib/smlnj/bin
 export PATH=$PATH:/afs/andrew/course/15/150/bin
 export PATH=$PATH:/afs/cs/academic/class/15210-s17/mlton-spoonhower/build/bin
 export PATH=$PATH:/usr/local/depot/ispc-v1.9.1-linux
+export PATH=$PATH:/usr/local/depot/cuda-8.0/bin
 
 # prompt/etc
 
