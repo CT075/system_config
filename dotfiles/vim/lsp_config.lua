fbd802d52d3d1af22cbe4889c850e3e4e0712169
Cameron Wong <cam@camdar.io>
2022-01-20 23:41:05 -0500

    vim updates, i guess, and also i3config

--- dotfiles/vim/lsp_config.lua
+++ dotfiles/vim/lsp_config.lua
@@ -1,6 +1,5 @@
 lspconfig = require'lspconfig'
 completion_callback = require'completion'.on_attach
 
-lspconfig.pyls.setup{on_attach=completion_callback}
 lspconfig.rust_analyzer.setup{on_attach=completion_callback}
 
