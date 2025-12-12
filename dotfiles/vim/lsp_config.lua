da4e000080285f13d2f401aae2662613d16c833b
Cameron Wong <cwong@g.harvard.edu>
2025-12-12 12:16:59 -0500

    pretender changes
    Signed-off-by: Cameron Wong <cwong@g.harvard.edu>

--- dotfiles/vim/lsp_config.lua
+++ dotfiles/vim/lsp_config.lua
@@ -1,5 +1,3 @@
-lspconfig = require'lspconfig'
-
 opts = {noremap=true, silent=true}
 
 vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
@@ -12,15 +10,27 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'J', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
 end
 
-lspconfig.rust_analyzer.setup{on_attach=on_attach}
-lspconfig.hls.setup{on_attach=on_attach}
-lspconfig.metals.setup{on_attach=on_attach}
-lspconfig.anakin_language_server.setup{
+local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "scala", "sbt", "java" },
+  callback = function()
+    metals = require("metals")
+    cfg = metals.bare_config()
+    cfg.init_options.statusBarProvider = "on"
+    cfg.settings = {
+      metalsBinaryPath = "/home/cam/.local/share/coursier/bin/metals"
+    }
+    metals.initialize_or_attach(cfg)
+    vim.api.nvim_set_keymap('n', '<localleader>t', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.api.nvim_set_keymap('n', 'J', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  end,
+  group = nvim_metals_group,
+})
+
+vim.lsp.config('rust_analyzer', {on_attach=on_attach})
+vim.lsp.config('hls', {
   on_attach=on_attach,
-  settings={
-    anakinls={
-      pyflakes_errors={ "ImportStarNotPermitted", "UndefinedExport", "UndefinedLocal", "UndefinedName", "DuplicateArgument", "MultiValueRepeatedKeyLiteral", "MultiValueRepeatedKeyVariable", "FutureFeatureNotDefined", "LateFutureImport", "ReturnOutsideFunction", "YieldOutsideFunction", "ContinueOutsideLoop", "BreakOutsideLoop", "TwoStarredExpressions", "TooManyExpressionsInStarredAssignment", "ForwardAnnotationSyntaxError", "RaiseNotImplemented", "StringDotFormatExtraPositionalArguments", "StringDotFormatExtraNamedArguments", "StringDotFormatMissingArgument", "StringDotFormatMixingAutomatic", "StringDotFormatInvalidFormat", "PercentFormatInvalidFormat", "PercentFormatMixedPositionalAndNamed", "PercentFormatUnsupportedFormat", "PercentFormatPositionalCountMismatch", "PercentFormatExtraNamedArguments", "PercentFormatMissingArgument", "PercentFormatExpectedMapping", "PercentFormatExpectedSequence", "PercentFormatStarRequiresSequence" },
-      mypy_enabled=true,
-    },
-  }
-}
+  settings={haskell={plugin={stan={globalOn=false}}}}
+})
+vim.lsp.config('pyright', {on_attach=on_attach})
+-- require('idris2').setup({server={on_attach=on_attach}})
