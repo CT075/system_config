a2ae0e57414d004baad4862992a18d40416fe85d
Cameron Wong <cameron@basis.ai>
2026-07-15 01:41:31 -0400

    add harpoon
    Signed-off-by: Cameron Wong <cameron@basis.ai>

--- dotfiles/vim/config.lua
+++ dotfiles/vim/config.lua
@@ -1,5 +1,3 @@
-treesitter = require('nvim-treesitter')
-
 require'nvim-treesitter'.install { 'latex', 'rust', 'haskell' }
 
 vim.api.nvim_create_autocmd('FileType', {
@@ -23,8 +21,17 @@ require'lualine'.setup {
   },
 }
 
-require'bufferline'.setup { options = {
-  numbers = "none",
-  show_buffer_icons = false,
-  show_buffer_close_icons = false,
-}}
+harpoon = require'harpoon'
+harpoon:setup({settings={save_on_toggle = true}})
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end,
+  {desc = "Harpoon add file"})
+vim.keymap.set("n", "<C-e>", function()
+    harpoon.ui:toggle_quick_menu(harpoon:list())
+  end, {desc = "Harpoon quick menu"})
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<C-k>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-j>", function() harpoon:list():next() end)
