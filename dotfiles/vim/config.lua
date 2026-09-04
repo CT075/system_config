e040c5e9afe0ab2aaf31eb147e80f9ec358e53a6
Cameron Wong <cwong@g.harvard.edu>
2026-09-04 11:08:24 -0400

    .
    Signed-off-by: Cameron Wong <cwong@g.harvard.edu>

--- dotfiles/vim/config.lua
+++ dotfiles/vim/config.lua
@@ -3,7 +3,7 @@ treesitter = require('nvim-treesitter')
 require'nvim-treesitter'.install { 'latex', 'rust', 'haskell' }
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'latex', 'rust', 'haskell' },
+  pattern = { 'latex', 'rust' },
   callback = function() vim.treesitter.start() end,
 })
 
